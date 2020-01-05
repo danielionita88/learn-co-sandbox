9c60e282f186ebe57d4a912356fed54934934bd4
Daniel I <danielionita88@yahoo.com>
2020-01-05 21:37:01 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,15 @@
+#puts "Joy to the world"
+#puts "All the boys and girls"
+#puts "Joy to the fishes in the deep blue sea"
+#puts "Joy to you and me"
+
+greeting = "friendly_greeting"
+
+case greeting
+when "unfriendly_greeting"
+  puts "Bye bye"
+  when "friendly_greeting"
+    puts "Hello"
+  end
+  
+  
